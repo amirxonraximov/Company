11486efb6d49e584c7d2afb13fbfa4b1585f61c0
Amirxon <arm19980201@gmaile.com>
2022-12-23 23:25:50 +0500

    initial commit

---
+++ src/main/java/com/example/lesson_1_task_1/Lesson1Task1Application.java
@@ -0,0 +1,14 @@
+package com.example.lesson_1_task_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson1Task1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Lesson1Task1Application.class, args);
+    }
+
+}
